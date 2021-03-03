23fe383e047a815ad88d5c8fa3ad9fe6ef8f30d8
schollsebastian <scholl.sebastian@gmx.at>
2021-03-03 17:59:40 +0100

    List trending movies in TableView

---
+++ SwipeMovies-iOS-Client/Movie.swift
@@ -0,0 +1,11 @@
+class Movie {
+    
+    init(_ title: String, _ overview: String) {
+        self.title = title
+        self.overview = overview
+    }
+    
+    var title: String
+    var overview: String
+    
+}
