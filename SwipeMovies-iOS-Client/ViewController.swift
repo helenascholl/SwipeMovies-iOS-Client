8583f1fac61198b07e4fda1ed80eb94035ef090b
schollsebastian <scholl.sebastian@gmx.at>
2021-04-29 00:19:49 +0200

    Implement joining groups

--- SwipeMovies-iOS-Client/ViewController.swift
+++ SwipeMovies-iOS-Client/ViewController.swift
@@ -2,10 +2,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let userId = 0
+    
     @IBOutlet weak var joinGroupText: UITextField!
     @IBOutlet weak var createGroupText: UITextField!
     
     @IBAction func joinGroupButton(_ sender: Any) {
+        if let id = joinGroupText.text, let url = URL(string: "\(getBackendUrl())/api/users/\(userId)/groups") {
+            let json: [String: Any] = [
+                "id": id
+            ]
+            
+            if let jsonData = try? JSONSerialization.data(withJSONObject: json) {
+                var request = URLRequest(url: url)
+                
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+                request.httpMethod = "POST"
+                request.httpBody = jsonData
+                
+                let task = URLSession.shared.dataTask(with: request)
+                task.resume()
+                
+                joinGroupText.text = ""
+            }
+        }
     }
     
     @IBAction func createGroupButton(_ sender: Any) {
