8c7c29e60c8cf6e20705a6ef28caaa193b3523ed
schollsebastian <scholl.sebastian@gmx.at>
2021-04-28 23:52:36 +0200

    Implement creating groups

--- SwipeMovies-iOS-Client/ViewController.swift
+++ SwipeMovies-iOS-Client/ViewController.swift
@@ -2,135 +2,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let queue = DispatchQueue(label: "download")
-    let posterBaseUrl = "https://image.tmdb.org/t/p/w200"
-    let userId = 0
+    @IBOutlet weak var joinGroupText: UITextField!
+    @IBOutlet weak var createGroupText: UITextField!
     
-    var movies: [Movie] = []
-    var currentIndex: Int = -1
-    var currentPage: Int = 1
-    var swipedRight: [Movie] = []
-    var swipedLeft: [Movie] = []
-
-    @IBOutlet weak var moviePoster: UIImageView!
-    @IBOutlet weak var movieTitle: UILabel!
-    @IBOutlet weak var movieDescription: UILabel!
-
-    @IBAction func buttonRight(_ sender: Any) {
-        swipeRight()
+    @IBAction func joinGroupButton(_ sender: Any) {
     }
     
-    @IBAction func buttonLeft(_ sender: Any) {
-        swipeLeft()
-    }
-    
-    @IBAction func swipeGestureRight(_ sender: Any) {
-        swipeRight()
-    }
-    
-    @IBAction func swipeGestureLeft(_ sender: Any) {
-        swipeLeft()
-    }
-    
-    func swipeRight() {
-        if currentIndex >= 0 {
-            print("Swiped right: \(movies[currentIndex].title)")
-            swipedRight.append(movies[currentIndex])
-            postSwipedMovie(SwipedMovie(movies[currentIndex], .right))
-            showNextMovie()
-        }
-    }
-    
-    func swipeLeft() {
-        if currentIndex >= 0 {
-            print("Swiped left: \(movies[currentIndex].title)")
-            swipedLeft.append(movies[currentIndex])
-            postSwipedMovie(SwipedMovie(movies[currentIndex], .left))
-            showNextMovie()
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addNewMovies() {
-            self.currentPage += 1
-            self.showNextMovie()
-        }
-    }
-    
-    func showNextMovie() {
-        currentIndex += 1
-        
-        if currentIndex == movies.count - 3 {
-            addNewMovies() {
-                self.currentPage += 1
-            }
-        }
-        
-        moviePoster.image = nil
-        movieTitle.text = movies[currentIndex].title
-        movieDescription.text = movies[currentIndex].description
-        
-        queue.async {
-            let posterUrl = URL(string: self.movies[self.currentIndex].posterUrl)
-            let imageData = try! Data(contentsOf: posterUrl!)
-            
-            DispatchQueue.main.async {
-                self.moviePoster.image = UIImage(data: imageData)
-            }
-        }
-    }
-    
-    func addNewMovies(_ callback: @escaping () -> ()) {
-        queue.async {
-            if let url = URL(string: "\(self.getBackendUrl())/api/users/\(self.userId)/movies") {
-                self.downloadMovies(url, callback)
-            } else {
-                print("Cannot resolve URL")
-            }
-        }
-    }
-    
-    func downloadMovies(_ url: URL, _ callback: @escaping () -> ()) {
-        if let data = try? Data(contentsOf: url) {
-            if let movies = try? JSONSerialization.jsonObject(with: data, options: []) as? [Any] {
-                var downloadedMovies: [Movie] = []
-                
-                for movie in movies {
-                    if let dict = movie as? [String: Any] {
-                        let id = dict["id"] as! Int
-                        let title = dict["title"] as! String
-                        let description = dict["description"] as! String
-                        let posterUrl = dict["posterUrl"] as! String
-                        
-                        downloadedMovies.append(Movie(id, title, description, posterUrl))
-                    }
-                }
-                
-                DispatchQueue.main.async {
-                    self.movies.append(contentsOf: downloadedMovies)
-                    callback()
-                }
-            }
-        } else {
-            print("Download failed")
-        }
-    }
-    
-    func getBackendUrl() -> String {
-        return Bundle.main.infoDictionary?["Backend URL"] as! String
-    }
-    
-    func postSwipedMovie(_ swipedMovie: SwipedMovie) {
-        if let url = URL(string: "\(getBackendUrl())/api/users/\(userId)/movies/swiped") {
+    @IBAction func createGroupButton(_ sender: Any) {
+        if let name = createGroupText.text, let url = URL(string: "\(getBackendUrl())/api/groups") {
             let json: [String: Any] = [
-                "movie": [
-                    "id": swipedMovie.movie.id,
-                    "title": swipedMovie.movie.title,
-                    "description": swipedMovie.movie.description,
-                    "posterUrl": swipedMovie.movie.posterUrl
-                ],
-                "swipeDirection": swipedMovie.swipeDirection == .right ? "right" : "left"
+                "name": name
             ]
             
             if let jsonData = try? JSONSerialization.data(withJSONObject: json) {
@@ -143,8 +24,18 @@ class ViewController: UIViewController {
                 
                 let task = URLSession.shared.dataTask(with: request)
                 task.resume()
+                
+                createGroupText.text = ""
             }
         }
     }
-
+    
+    @IBAction func showSwipe(_ sender: Any) {
+        performSegue(withIdentifier: "swipe", sender: self)
+    }
+    
+    func getBackendUrl() -> String {
+        return Bundle.main.infoDictionary?["Backend URL"] as! String
+    }
+    
 }
