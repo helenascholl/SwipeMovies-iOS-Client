d5f8405c17767f29b1a95cf316d16c840660cb33
schollsebastian <scholl.sebastian@gmx.at>
2021-05-18 20:51:51 +0200

    Create class User

---
+++ SwipeMovies-iOS-Client/User.swift
@@ -0,0 +1,11 @@
+class User {
+    
+    init(_ id: Int, _ username: String) {
+        self.id = id
+        self.username = username
+    }
+    
+    var id: Int
+    var username: String
+    
+}
