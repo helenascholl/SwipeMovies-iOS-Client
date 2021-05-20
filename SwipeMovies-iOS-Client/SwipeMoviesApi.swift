045cfe14a00a74f6c3f9826122452799bc9f1ed3
schollsebastian <scholl.sebastian@gmx.at>
2021-05-20 09:38:11 +0200

    Make created group param of createGroup() callback

--- SwipeMovies-iOS-Client/SwipeMoviesApi.swift
+++ SwipeMovies-iOS-Client/SwipeMoviesApi.swift
@@ -17,15 +17,35 @@ class SwipeMoviesApi {
         return instance!
     }
     
-    public func createGroup(_ name: String, _ callback: @escaping () -> Void) {
-        if let user = user, let url = URL(string: "\(getBackendUrl())/api/groups") {
+    public func createGroup(_ name: String, _ callback: @escaping (_ group: Group) -> Void) {
+        if let url = URL(string: "\(getBackendUrl())/api/groups") {
             let json: [String: Any] = [
-                "user": [
-                    "id": user.id
-                ],
-                "group": [
-                    "name": name
-                ]
+                "name": name
+            ]
+            
+            if let jsonData = try? JSONSerialization.data(withJSONObject: json) {
+                var request = URLRequest(url: url)
+                
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+                request.httpMethod = "POST"
+                request.httpBody = jsonData
+                
+                let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, _, _) -> Void in
+                    if let data = data, let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        let group = Group(json["id"] as! Int, json["name"] as! String)
+                        callback(group)
+                    }
+                })
+                task.resume()
+            }
+        }
+    }
+    
+    public func joinGroup(_ id: Int, _ callback: @escaping () -> Void) {
+        if let user = user, let url = URL(string: "\(getBackendUrl())/api/users/\(user.id)/groups") {
+            let json: [String: Any] = [
+                "id": id
             ]
             
             if let jsonData = try? JSONSerialization.data(withJSONObject: json) {
