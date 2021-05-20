e2636d4690f26046b23f0140cbe33726dfc32f3a
schollsebastian <scholl.sebastian@gmx.at>
2021-05-20 08:52:52 +0200

    Implement SwipeMoviesApi.createGroup()

--- SwipeMovies-iOS-Client/SwipeMoviesApi.swift
+++ SwipeMovies-iOS-Client/SwipeMoviesApi.swift
@@ -17,6 +17,33 @@ class SwipeMoviesApi {
         return instance!
     }
     
+    public func createGroup(_ name: String, _ callback: @escaping () -> Void) {
+        if let user = user, let url = URL(string: "\(getBackendUrl())/api/groups") {
+            let json: [String: Any] = [
+                "user": [
+                    "id": user.id
+                ],
+                "group": [
+                    "name": name
+                ]
+            ]
+            
+            if let jsonData = try? JSONSerialization.data(withJSONObject: json) {
+                var request = URLRequest(url: url)
+                
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+                request.httpMethod = "POST"
+                request.httpBody = jsonData
+                
+                let task = URLSession.shared.dataTask(with: request, completionHandler: { (_, _, _) -> Void in
+                    callback()
+                })
+                task.resume()
+            }
+        }
+    }
+    
     private func getBackendUrl() -> String {
         return Bundle.main.infoDictionary?["Backend URL"] as! String
     }
