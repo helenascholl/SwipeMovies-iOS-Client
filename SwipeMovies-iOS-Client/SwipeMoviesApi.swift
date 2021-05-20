adb4dfdd44a8bae06fb95dec20fa0f7ec98440b5
schollsebastian <scholl.sebastian@gmx.at>
2021-05-20 08:35:38 +0200

    Add user field

--- SwipeMovies-iOS-Client/SwipeMoviesApi.swift
+++ SwipeMovies-iOS-Client/SwipeMoviesApi.swift
@@ -1,8 +1,11 @@
 class SwipeMoviesApi {
     
     private static var instance: SwipeMoviesApi?
+    private var user: User?
     
-    private init() {}
+    private init() {
+        user = User(0, "Test")
+    }
 
     public static func getInstance() -> SwipeMoviesApi {
         if instance === nil {
