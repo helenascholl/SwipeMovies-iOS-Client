5e6019dd601439ff94d731b66c81138c950d5107
schollsebastian <scholl.sebastian@gmx.at>
2021-06-17 09:10:45 +0200

    Use SwipeMoviesApi in MatchViewController

--- SwipeMovies-iOS-Client/MatchViewController.swift
+++ SwipeMovies-iOS-Client/MatchViewController.swift
@@ -3,11 +3,13 @@ import UIKit
 class MatchViewController: UITableViewController {
     
     var matches: [Match] = []
-    var userId = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.download()
+        SwipeMoviesApi.getInstance().getMatches({ (_ matches: [Match]) -> Void in
+            self.matches = matches
+            self.tableView.reloadData()
+        })
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -31,44 +33,6 @@ class MatchViewController: UITableViewController {
         cell.detailTextLabel?.text = String(detailText.dropLast(2))
 
         return cell
-    }
-    
-    func download() {
-        var downloadedMatches: [Match] = []
-        
-        if let url = URL(string: "\(getBackendUrl())/api/users/\(userId)/matches") {
-            if let data = try? Data(contentsOf: url) {
-                if let json = try? JSONSerialization.jsonObject(with: data, options: []), let array = json as? [Any] {
-                    for obj in array {
-                        if let dict = obj as? [String: Any] {
-                            if let movie = dict["movie"] as? [String: Any] {
-                                let match = Match(Movie(movie["id"] as! Int, movie["title"] as! String, movie["description"] as! String, movie["posterUrl"] as! String))
-                                
-                                if let users = dict["users"] as? [Any] {
-                                    for userObj in users {
-                                        if let user = userObj as? [String: Any] {
-                                            match.addUser(User(user["id"] as! Int, user["username"] as! String))
-                                        }
-                                    }
-
-                                    downloadedMatches.append(match)
-                                }
-                            }
-                        }
-                    }
-                    
-                    DispatchQueue.main.async {
-                        self.matches = downloadedMatches
-                        self.tableView.reloadData()
-                    }
-                }
-            } else {
-                print("Download failed")
-            }
-        } else {
-            print("Cannot resolve URL")
-        }
-        
     }
     
     func getBackendUrl() -> String {
